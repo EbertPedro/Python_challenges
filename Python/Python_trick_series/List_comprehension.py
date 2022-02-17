31f0174d70ee820f6d850a668dfb4b842434ec53
EbertPedro <echatem@uni.pe>
2022-02-16 23:51:01 -0500

    List comprehension with filter: if statment

--- Python/Python_trick_series/List_comprehension.py
+++ Python/Python_trick_series/List_comprehension.py
@@ -1,12 +1,4 @@
 #Methode through List comprehension
-squares = [x*x for x in range(10)]
-print(squares)
-
-squares2 = []
-for x in range(10):
-    squares2.append(x*x)
-print(squares2)
-print("Third....")
 """
     Methode generalized with List comprenhension:
     list = [Expresion(x) for (values) in (collection or iterable)]
@@ -16,3 +8,15 @@ print("Third....")
     for (values) in (collection or iterable):
         list.append(Expresion(values))
 """
+squares = [x*x for x in range(10)]
+print(squares)
+
+squares2 = []
+for x in range(10):
+    squares2.append(x*x)
+print(squares2)
+print("Third....")
+
+#List comprehension: Filtering applying if condition:
+even_values = [x*x for x in range(10) if x == 2]
+print(even_values)
