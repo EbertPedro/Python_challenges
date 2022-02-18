e9e1e52ca52bb69280975d5e95e2469175b29f8a
EbertPedro <echatem@uni.pe>
2022-02-17 21:58:31 -0500

    Coded cleaned and was added readable descriptions

--- Python/Python_trick_series/Yield.py
+++ Python/Python_trick_series/Yield.py
@@ -1,17 +1,28 @@
 #We are going to approach in the generator Yield 
 
+from unittest import result
+
+
 my_list = [x for x in range(1,10)]
-print(my_list)
+print("My current list: ",my_list)
 
 #Without generator
 def process_list(my_list):
+    result = [] #We are copy the values in an different address memory with the same name
     len_list = len(my_list) 
     for i in range(len_list):
-        my_list[i] *= 2
-    return my_list[i]
+        result.append(my_list[i] * 2)
+    return result
 
 #With generator
 def process_list_generator(my_list):
     len_list = len(my_list)
     for i in range(len_list):
-        return my_list[i] * 2
+        yield i, my_list[i] * 2
+
+for item in process_list(my_list):
+    print(item, end=" ")
+
+print(" ")
+for index, item in process_list_generator(my_list):
+    print(index, item)
