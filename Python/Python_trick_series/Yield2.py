0b5c63aab1d76f3da391ed9826cb095e283b20fd
EbertPedro <echatem@uni.pe>
2022-02-17 22:52:54 -0500

    Were added an example to print out yield return objects

--- Python/Python_trick_series/Yield2.py
+++ Python/Python_trick_series/Yield2.py
@@ -4,6 +4,9 @@ the return and yield operator in python. The key idea is
 the memory and the time taken to execute both generators.
 """
 
+from traceback import print_tb
+
+
 def square_numbers(nums):
     result = []
     for item in nums:
@@ -19,3 +22,15 @@ my_nums = square_numbers([1,2,3,4,5])
 print(my_nums)
 my_nums2 = square_numbers2([1,2,3,4,5])
 print(my_nums2)
+
+#The Yield object cannot show something because it only 
+#show us when we print out by foor loop or an iterator
+#we can print out one at a time by next or foor loop
+# print(next(my_nums2))
+# print(next(my_nums2))
+# print(next(my_nums2))
+# print(next(my_nums2))
+# print(next(my_nums2))
+
+for item in my_nums2:
+    print(item, end=" ")
