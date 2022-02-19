bc83c1e2b66ff5e68400a3259c203c347f5268a3
EbertPedro <echatem@uni.pe>
2022-02-19 18:44:44 -0500

    Added an example for see the difference between an function and map object

--- Python/Python_trick_series/Lambda-map-reduce-filter/map.py
+++ Python/Python_trick_series/Lambda-map-reduce-filter/map.py
@@ -1,7 +1,18 @@
 """
 Apply same function to each element of an sequence
 and return the modified list.
-Take as imput: List [m,n,p] and Function f()
+Take as imput: List [m,n,p] or an iterable and Function f()
 Output: New list [f(m),f(n),f(p)]
+In implementation: list(map(f(),[m,n,p]))
 """
 
+#We get the square of the list [4,3,2,1] by function and map
+lst = [4,3,2,1]
+def square(lst):
+    lst2 = []
+    for num in lst:
+        lst2.append(num*num)
+    return lst2
+
+map_square = list(map(lambda x:x*x,lst))
+print(square(lst), map_square)
