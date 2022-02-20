f5ca413859e880540ffa2c9e78ce2602f5546105
EbertPedro <echatem@uni.pe>
2022-02-19 19:20:10 -0500

    We added more examples with map

--- Python/Python_trick_series/Lambda-map-reduce-filter/map.py
+++ Python/Python_trick_series/Lambda-map-reduce-filter/map.py
@@ -1,25 +1,25 @@
 """
 Apply same function to each element of an sequence
 and return the modified list.
-Take as imput: List [m,n,p] or an iterable and Function f()
+Take as imput: an iterable(set,tuples,list[m,n,p]) and Function f()
 Output: New map object (f(m),f(n),f(p))
 In implementation to get the values: list(map(f(),[m,n,p]))
 """
 
 #We get the square of the list [4,3,2,1] by function and map
-lst = [4,3,2,1]
-def square(lst):
-    lst2 = []
-    for num in lst:
-        lst2.append(num*num)
-    return lst2
+numbers = [4,3,2,1]
+numbers2 = [6,7,10,2]
+def square(num):
+    return num*num
 
-map_square = map(lambda x:x*x,lst)
-print(square(lst))
+map_square_iterator = map(lambda x:x*x,numbers)
+func_square_iterator = map(square,numbers)
+map_sum_iterator = map(lambda x1,x2: x1+x2, numbers,numbers2)
 
 #We show an item one at time
-print(next(map_square))
-print(next(map_square))
-print(next(map_square))
-print(next(map_square))
+# print(next(func_square_iterator))
+print(next(map_square_iterator)," ",next(func_square_iterator)," ",next(map_sum_iterator))
+print(next(map_square_iterator)," ",next(func_square_iterator)," ",next(map_sum_iterator))
+print(next(map_square_iterator)," ",next(func_square_iterator)," ",next(map_sum_iterator))
+print(next(map_square_iterator)," ",next(func_square_iterator)," ",next(map_sum_iterator))
 # print(list(map_square)) 
