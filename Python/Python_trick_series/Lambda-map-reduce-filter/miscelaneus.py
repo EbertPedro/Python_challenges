efebbe2cf54d30b555ac1a76deaf672442f194d1
EbertPedro <echatem@uni.pe>
2022-02-19 20:47:47 -0500

    Were added an example with iter() customized

--- Python/Python_trick_series/Lambda-map-reduce-filter/miscelaneus.py
+++ Python/Python_trick_series/Lambda-map-reduce-filter/miscelaneus.py
@@ -1,36 +1,36 @@
-# list of vowels
-vowels = ['a', 'e', 'i', 'o', 'u']
-vowels_iter = iter(vowels)
+# # list of vowels
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# vowels_iter = iter(vowels)
 
-print(next(vowels_iter))    # 'a'
-print(next(vowels_iter))    # 'e'
-print(next(vowels_iter))    # 'i'
-print(next(vowels_iter))    # 'o'
-# print(next(vowels_iter))    # 'u'
-print(vowels_iter.__next__()) 
+# print(next(vowels_iter))    # 'a'
+# print(next(vowels_iter))    # 'e'
+# print(next(vowels_iter))    # 'i'
+# print(next(vowels_iter))    # 'o'
+# # print(next(vowels_iter))    # 'u'
+# print(vowels_iter.__next__()) 
 
 
-# class PrintNumber:
-#     def __init__(self, max):
-#         self.max = max
+class PrintNumber:
+    def __init__(self, max):
+        self.max = max
 
-#     def __iter__(self):
-#         self.num = 0
-#         return self
+    def __iter__(self):
+        self.num = 0
+        return self
 
-#     def __next__(self):
-#         if(self.num >= self.max):
-#             raise StopIteration
-#         self.num += 1
-#         return self.num
+    def __next__(self):
+        if(self.num >= self.max):
+            raise StopIteration
+        self.num += 1
+        return self.num
 
-# print_num = PrintNumber(3)
+print_num = PrintNumber(3)
 
-# # print_num_iter = iter(print_num)
-# print_num_iter = print_num.__iter__()
-# print(next(print_num_iter))  # 1
-# print(next(print_num_iter))  # 2
-# print(next(print_num_iter))  # 3
+# print_num_iter = iter(print_num)
+print_num_iter = print_num.__iter__()
+print(next(print_num_iter))  # 1
+print(next(print_num_iter))  # 2
+print(next(print_num_iter))  # 3
 
-# # raises StopIteration
-# # print(next(print_num_iter))
+# raises StopIteration
+# print(next(print_num_iter))
