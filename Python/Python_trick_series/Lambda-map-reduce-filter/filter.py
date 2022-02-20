f8e630d54d3d4def7607116b81c84b5f06190acc
EbertPedro <echatem@uni.pe>
2022-02-19 19:52:58 -0500

    Cleaned and readable

--- Python/Python_trick_series/Lambda-map-reduce-filter/filter.py
+++ Python/Python_trick_series/Lambda-map-reduce-filter/filter.py
@@ -6,15 +6,15 @@ Output: New map object (f(m),f(n),f(p)) --> if each element satisfy the conditio
 In implementation to get the values by: list(map(f(),[m,n,p]))
 """
 
-# #We get the square of the list [4,3,2,1] by function and map
-# numbers = [4,3,2,1]
-# numbers2 = [6,7,10,2]
-# def square(num):
-#     return num%2 != 0
+#We get the square of the list [4,3,2,1] by function and map
+numbers = [4,3,2,1]
+numbers2 = [6,7,10,2]
+def square(num):
+    return num%2 != 0
 
-# map_square_iterator = filter(lambda x:x%2==0,numbers2)
-# func_square_iterator = filter(square,numbers2)
+map_square_iterator = filter(lambda x:x%2==0,numbers2)
+func_square_iterator = filter(square,numbers2)
 
-# #We show an item one at time
-# print(list(map_square_iterator))
-# print(list(func_square_iterator)) 
+#We show an item one at time
+print(list(map_square_iterator))
+print(list(func_square_iterator)) 
