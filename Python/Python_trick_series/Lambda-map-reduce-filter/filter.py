e4d3a5f84698afc6dddaff4b8a753865957de43d
EbertPedro <echatem@uni.pe>
2022-02-19 19:49:44 -0500

    Were initialized the description of the filter function

---
+++ Python/Python_trick_series/Lambda-map-reduce-filter/filter.py
@@ -0,0 +1,21 @@
+"""
+Apply same function to each element of an sequence
+and return the element if it satisfy the condition of the function.
+Take as imput: an Function f() and an iterable(set,tuples,list[m,n,p])
+Output: New map object (f(m),f(n),f(p)) --> if each element satisfy the condition of f()
+In implementation to get the values by: list(map(f(),[m,n,p]))
+"""
+
+#We get the square of the list [4,3,2,1] by function and map
+numbers = [4,3,2,1]
+numbers2 = [6,7,10,2]
+def square(num):
+    return num%2 != 0
+
+map_square_iterator = filter(lambda x:x%2==0,numbers2)
+func_square_iterator = filter(square,numbers2)
+# map_sum_iterator = filter(lambda x1,x2: x1+x2, numbers,numbers2)
+
+#We show an item one at time
+print(list(map_square_iterator))
+print(list(func_square_iterator)) 
