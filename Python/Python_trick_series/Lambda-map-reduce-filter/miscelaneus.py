7a625e1734b1dc6a905c8a0d91171617a51ad3fb
EbertPedro <echatem@uni.pe>
2022-02-19 21:44:07 -0500

    Were added an example of open() function

--- Python/Python_trick_series/Lambda-map-reduce-filter/miscelaneus.py
+++ Python/Python_trick_series/Lambda-map-reduce-filter/miscelaneus.py
@@ -1,3 +1,4 @@
+#*************first*******************
 # # list of vowels
 # vowels = ['a', 'e', 'i', 'o', 'u']
 # vowels_iter = iter(vowels)
@@ -9,28 +10,35 @@
 # # print(next(vowels_iter))    # 'u'
 # print(vowels_iter.__next__()) 
 
+#***********second**********************
+# class PrintNumber:
+#     def __init__(self, max):
+#         self.max = max
 
-class PrintNumber:
-    def __init__(self, max):
-        self.max = max
+#     def __iter__(self):
+#         self.num = 0
+#         return self
 
-    def __iter__(self):
-        self.num = 0
-        return self
+#     def __next__(self):
+#         if(self.num >= self.max):
+#             raise StopIteration
+#         self.num += 1
+#         return self.num
 
-    def __next__(self):
-        if(self.num >= self.max):
-            raise StopIteration
-        self.num += 1
-        return self.num
+# print_num = PrintNumber(3)
 
-print_num = PrintNumber(3)
+# # print_num_iter = iter(print_num)
+# print_num_iter = print_num.__iter__()
+# print(next(print_num_iter))  # 1
+# print(next(print_num_iter))  # 2
+# print(next(print_num_iter))  # 3
 
-# print_num_iter = iter(print_num)
-print_num_iter = print_num.__iter__()
-print(next(print_num_iter))  # 1
-print(next(print_num_iter))  # 2
-print(next(print_num_iter))  # 3
+# # raises StopIteration
+# # print(next(print_num_iter))
 
-# raises StopIteration
-# print(next(print_num_iter))
+#*************third******************
+f = open("unbalanced_data.txt","w")
+f.write("This my first code with write function!!\n")
+f.write("This my first code with write function2!!\n")
+
+# f.writelines("Added at the last line of code")
